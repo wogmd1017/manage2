a7288d0d71a73fbf4f7b908c8bcc18f2e9645023
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2024-03-13 09:12:37 +0900

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 @echo off
 cd C:\Windows\System32\drivers\etc
-wget -N https://raw.githubusercontent.com/wogmd1017/manage/main/hosts
+wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hosts
