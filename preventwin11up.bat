57cf7310659f7d7ed0190f6652b34b903b29260a
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2024-06-20 00:56:04 +0900

    Update preventwin11up.bat

--- preventwin11up.bat
+++ preventwin11up.bat
@@ -1,4 +1,6 @@
 CD c:\windows\system32
 REM prevent upgrade win11
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseversion /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseversion" /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ProductVersion" /t REG_SZ /d "Windows 10" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "TargetReleaseversionInfo" /t REG_SZ /d "21H2" /f
+gpupdate /force
