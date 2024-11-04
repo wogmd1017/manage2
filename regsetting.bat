c2ba3500d9d1eee28c7e1f09df8cc1ea45e17c50
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2024-11-04 09:50:58 +0900

    Update regsetting.bat

--- regsetting.bat
+++ regsetting.bat
@@ -103,6 +103,8 @@ REM Disable URI
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableAppUriHandlers" /t REG_DWORD /d 0 /f
 REM Disable windows ink
 reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\WindowsInkWorkspace" /v "AllowWindowsInkWorkspace" /t REG_DWORD /d 0 /f
+REM Disable power button
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PolicyManager\default\Start" /v "HidePowerButton” /t REG_DWORD /d 1 /f
 REM Disable CMD
 REM reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows" /v "DisableCMD" /t REG_DWORD /d 2 /f
 REM Disable Regedit
