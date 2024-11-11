c05faba4a657644825a8121f2fe1a21b60adbcea
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2024-11-11 20:01:52 +0900

    Update regupdate.bat

--- regupdate.bat
+++ regupdate.bat
@@ -1,3 +1,5 @@
 cd C:\Users\Administrator\Desktop\Data
+wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/au.bat
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/regsetting.bat
+au.bat
 regsetting.bat
