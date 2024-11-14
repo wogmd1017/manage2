d0eba24a104420e2ec618ec2d27e007dcb3e7b17
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2024-11-14 21:00:02 +0900

    Add files via upload

---
+++ cPCsetting1.bat
@@ -0,0 +1,64 @@
+@echo off
+set A1=0
+set A2=0
+set A3=0
+set A4=0
+set A5=0
+
+:top
+cd c:\windows\system32
+echo before run : Uninstall V3
+echo 1. Delete Start App lists, %A1% times runed!
+echo 2. Desktop deny setting, %A2% times runed!
+echo 3. Schedule start, %A3% times runed!
+echo 4. HKU resitry setting, %A4% times runed!
+echo 5. HKLM resitry setting, %A5% times runed!
+set /p x=Choose work number:
+
+goto %x%
+
+:1
+del C:\Windows\System32\mspaint.exe
+del C:\Windows\System32\SnippingTool.exe
+cd C:\ProgramData\Microsoft\Windows\Start Menu
+rd /s /q Programs
+FOR %%a in (01,02,03,04,05,06,07,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30) DO (
+cd C:\Users\%%a\AppData\Local\Microsoft\Windows
+rd /s /q WinX
+cd C:\Users\%%a\AppData\Roaming\Microsoft\Windows\Start Menu
+rd /s /q Programs
+)
+pause
+set /a A1+=1
+goto top
+
+:2
+FOR %%b in (01,02,03,04,05,06,07,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30) DO (
+ICACLS C:\Users\%%b\Desktop /deny %%b:W
+)
+pause
+set /a A2+=1
+goto top
+
+:3
+schtasks /create /tn "hostup" /tr C:\Users\Administrator\Desktop\data\update.bat /sc minute
+schtasks /create /tn "hisup" /tr C:\Users\Administrator\Desktop\data\hisup.bat /sc minute
+pause
+set /a A3+=1
+goto top
+
+:4
+cd C:\Users\Administrator\Desktop\Data
+wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hku1.bat
+call hku1.bat
+pause
+set /a A4+=1
+goto top
+
+:5
+cd C:\Users\Administrator\Desktop\Data
+wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hklm1.bat
+call hklm1.bat
+pause
+set /a A5+=1
+goto top
