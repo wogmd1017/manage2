4d7ec6d2249bc8ab4a208411a851549d0753225b
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2024-11-14 15:56:49 +0900

    Update rdstart.bat

--- rdstart.bat
+++ rdstart.bat
@@ -1,2 +1,9 @@
-rd /s /q C:\ProgramData\Microsoft\Windows\Start Menu\Programs
+cd C:\ProgramData\Microsoft\Windows\Start Menu
+rd /s /q Programs
+FOR %%a in (01,02,03,04,05,06,07,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30) DO (
+cd C:\Users\%%a\AppData\Local\Microsoft\Windows
+rd /s /q WinX
+cd C:\Users\%%a\AppData\Roaming\Microsoft\Windows\Start Menu
+rd /s /q Programs
+)
 timeout /t 5
