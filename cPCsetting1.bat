5116effdb438e11debd4616f0924392995875774
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2024-11-15 21:34:09 +0900

    Update cPCsetting1.bat

--- cPCsetting1.bat
+++ cPCsetting1.bat
@@ -23,9 +23,9 @@ del C:\Windows\System32\SnippingTool.exe
 cd C:\ProgramData\Microsoft\Windows\Start Menu
 rd /s /q Programs
 FOR %%b in (01,02,03,04,05,06,07,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30) DO (
-cd C:\Users\%%a\AppData\Local\Microsoft\Windows
+cd C:\Users\%%b\AppData\Local\Microsoft\Windows
 rd /s /q WinX
-cd C:\Users\%%a\AppData\Roaming\Microsoft\Windows\Start Menu
+cd C:\Users\%%b\AppData\Roaming\Microsoft\Windows\Start Menu
 rd /s /q Programs
 )
 pause
@@ -34,7 +34,7 @@ goto top
 
 :2
 FOR %%c in (01,02,03,04,05,06,07,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30) DO (
-ICACLS C:\Users\%%b\Desktop /deny %%b:W
+ICACLS C:\Users\%%c\Desktop /deny %%c:W
 )
 pause
 set /a A2+=1
