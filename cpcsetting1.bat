6a83aa1f031dcf542ac229653fdbb6438fa8771b
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2024-11-18 08:40:34 +0900

    Update cpcsetting1.bat

--- cpcsetting1.bat
+++ cpcsetting1.bat
@@ -6,7 +6,9 @@ set A4=0
 set A5=0
 
 :top
+@echo off
 cd c:\windows\system32
+echo ====================================================================================================
 echo before run : Uninstall V3
 echo 1. Delete Start App lists, %A1% times runed!
 echo 2. Desktop deny setting, %A2% times runed!
@@ -14,10 +16,12 @@ echo 3. Schedule start, %A3% times runed!
 echo 4. HKU resitry setting, %A4% times runed!
 echo 5. HKLM resitry setting, %A5% times runed!
 set /p x=Choose work number:
+echo ====================================================================================================
 
 goto %x%
 
 :1
+@echo on
 del C:\Windows\System32\mspaint.exe
 del C:\Windows\System32\SnippingTool.exe
 cd C:\ProgramData\Microsoft\Windows\Start Menu
@@ -33,6 +37,7 @@ set /a A1+=1
 goto top
 
 :2
+@echo on
 FOR %%c in (01,02,03,04,05,06,07,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30) DO (
 ICACLS C:\Users\%%c\Desktop /deny %%c:W
 )
@@ -41,6 +46,7 @@ set /a A2+=1
 goto top
 
 :3
+@echo on
 schtasks /create /tn "hostup" /tr C:\Users\Administrator\Desktop\data\update.bat /sc minute
 schtasks /create /tn "hisup" /tr C:\Users\Administrator\Desktop\data\hisup.bat /sc minute
 pause
@@ -48,6 +54,7 @@ set /a A3+=1
 goto top
 
 :4
+@echo on
 cd C:\Users\Administrator\Desktop\Data
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hku1.bat
 call hku1.bat
@@ -56,6 +63,7 @@ set /a A4+=1
 goto top
 
 :5
+@echo on
 cd C:\Users\Administrator\Desktop\Data
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hklm1.bat
 call hklm1.bat
