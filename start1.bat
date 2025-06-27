b8c948213de735c4e7b7fc4706a3b2dc4e016e6f
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2025-06-27 12:21:35 +0900

    Update start1.bat

--- start1.bat
+++ start1.bat
@@ -1,5 +1,10 @@
 @echo off
+set "zipFile=browsinghistoryview.zip"
+set "destinationFolder=C:\windows\system32"
 cd C:\Users\Administrator\Desktop\Data
+wget -N https://www.nirsoft.net/utils/browsinghistoryview.zip
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/cpcsetting1.bat
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hostup.bat
+"C:\Program Files\Bandizip\bandizip.exe" x "%zipFile%" -o:"%destinationFolder%"
+pause
 cpcsetting1.bat
