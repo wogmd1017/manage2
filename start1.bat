31402b0753ec5bc119ea976d3637d0b996b50c0b
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2025-06-27 12:15:27 +0900

    Update start1.bat

--- start1.bat
+++ start1.bat
@@ -1,4 +1,5 @@
 @echo off
 cd C:\Users\Administrator\Desktop\Data
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/cpcsetting1.bat
+wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hostup.bat
 cpcsetting1.bat
