2cdb7be86730810ed3d9fafa77b89cb259df8d77
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2025-06-30 12:15:35 +0900

    Update start1.bat

--- start1.bat
+++ start1.bat
@@ -1,6 +1,6 @@
 @echo off
 set "zipFile=browsinghistoryview.zip"
-set "destinationFolder=C:\windows\system32"
+set "destinationFolder=C:\Users\Administrator\Desktop\Data"
 cd C:\Users\Administrator\Desktop\Data
 wget -N https://www.nirsoft.net/utils/browsinghistoryview.zip
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/cpcsetting1.bat
