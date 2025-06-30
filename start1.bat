e5abee61a1834f4483f967e81fb588e90e671706
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2025-06-30 14:12:09 +0900

    Update start1.bat

--- start1.bat
+++ start1.bat
@@ -2,7 +2,7 @@
 set "zipFile=browsinghistoryview.zip"
 set "destinationFolder=C:\Users\Administrator\Desktop\Data"
 cd C:\Users\Administrator\Desktop\Data
-wget -N https://www.nirsoft.net/utils/browsinghistoryview.zip
+wget -N --no-check-certificate https://www.nirsoft.net/utils/browsinghistoryview.zip
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/cpcsetting1.bat
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hostup.bat
 bandizip.exe x -y -aoa "%zipFile%"
