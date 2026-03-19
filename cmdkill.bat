e8cb5d787eadc27b38e2d38920845db5a4e651a4
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2026-03-20 06:15:59 +0900

    Add taskkill for mspaint and comment out old code

--- cmdkill.bat
+++ cmdkill.bat
@@ -5,18 +5,18 @@ set "ADMIN_NAME=%USERNAME%"
 taskkill /F /FI "IMAGENAME eq cmd.exe" /FI "USERNAME ne %ADMIN_NAME%" /T >nul 2>&1
 taskkill /F /FI "IMAGENAME eq regedit.exe" /FI "USERNAME ne %ADMIN_NAME%" /T >nul 2>&1
 taskkill /F /FI "IMAGENAME eq powershell.exe" /FI "USERNAME ne %ADMIN_NAME%" /T >nul 2>&1
+taskkill /F /FI "IMAGENAME eq mspaint.exe" /FI "USERNAME ne %ADMIN_NAME%" /T >nul 2>&1
 
-//for /L %%N in (1, 1, 40) do (
-//    set "ID=0%%N"
-//    set "USER_ID=!ID:~-2!"
-//    :: 바탕화면 경로 (서버 환경에 따라 C:\Users\!USER_ID!\Desktop 확인 필요)
-//    if exist "C:\Users\!USER_ID!\Desktop" (
-//        for /d %%D in ("C:\Users\!USER_ID!\Desktop\*새 폴더*") do (
-//            rd /s /q "%%D" >nul 2>&1
-//            echo [ACTION] Deleted dummy folder from Student !USER_ID!
-//        )
-//    )
-//)
+REM for /L %%N in (1, 1, 40) do (
+REM set "ID=0%%N"
+REM set "USER_ID=!ID:~-2!"
+REM if exist "C:\Users\!USER_ID!\Desktop" (
+REM for /d %%D in ("C:\Users\!USER_ID!\Desktop\*새 폴더*") do (
+REM rd /s /q "%%D" >nul 2>&1
+REM echo [ACTION] Deleted dummy folder from Student !USER_ID!
+REM )
+REM )
+REM )
 
 timeout /t 3 /nobreak >nul
 goto :LOOP
