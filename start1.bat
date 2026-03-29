29af8697f1cc08b209800276ef7611dcaba6b134
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2026-03-29 14:48:38 +0900

    Update start1.bat

--- start1.bat
+++ start1.bat
@@ -1,10 +1,17 @@
 @echo off
-set "zipFile=browsinghistoryview.zip"
-set "destinationFolder=C:\Users\Administrator\Desktop\Data"
-cd C:\Users\Administrator\Desktop\Data
+set "bhvZip=browsinghistoryview.zip"
+set "rcZip=rclone.zip"
+set "destDir=C:\Users\Administrator\Desktop\Data"
+cd /d "%destDir%"
 wget -N --no-check-certificate https://www.nirsoft.net/utils/browsinghistoryview.zip
+wget -N --no-check-certificate https://downloads.rclone.org/v1.73.3/rclone-v1.73.3-windows-amd64.zip -O %rcZip%
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/cpcsetting1.bat
 wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hostup.bat
-bandizip.exe x -y -aoa "%zipFile%"
+bandizip.exe x -y -aoa "%bhvZip%"
+bandizip.exe x -y -aoa -target:name "%rcZip%"
+move /y "rclone-v1.73.3-windows-amd64\rclone.exe" .
+rd /s /q "rclone-v1.73.3-windows-amd64"
+del /q "%bhvZip%"
+del /q "%rcZip%"
 pause
 cpcsetting1.bat
