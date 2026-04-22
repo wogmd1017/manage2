530d2a9014db50e84ba58f6433b3a5d5607fd696
wogmd1017 <87552444+wogmd1017@users.noreply.github.com>
2026-04-22 11:22:15 +0900

    Update hostup.bat

--- hostup.bat
+++ hostup.bat
@@ -1,3 +1,3 @@
 @echo off
 cd C:\Windows\System32\drivers\etc
-wget -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hosts
+"C:\Users\Administrator\Desktop\data\wget.exe" -N https://raw.githubusercontent.com/wogmd1017/manage2/main/hosts
